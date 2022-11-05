d364760c88d7a6144549791f61beff2444ccc263
Nolan Chu <nolancchu@gmail.com>
2022-11-05 15:10:06 -0700

    UpdateData.txt

--- dataProcessing.js
+++ dataProcessing.js
@@ -1,2 +1,7 @@
 //create k with " `data` "
-j=k.split("\n").map(x=>x.trim()).filter(x=>x!=="").map(line=>line.split("\t")).map(line=>line.join(" ")).join("\n")
+j = k.split("\n").map(x => x.trim()).filter(x => x !== "").map(line => line.split("\t")).map(line => line.join(" ")).join("\n")
+
+
+j = k.split("\n").map(x => x.trim()).filter(x => x !== "").map(line => line.split("\t")).map(line => { var i = line[6].split(" ").findIndex(x => x.startsWith("http")); var url = line[6].split(" ")[i]; console.log(url); line[6] = url; return line }).map(line => line.join(",")).join("\n")
+
+
